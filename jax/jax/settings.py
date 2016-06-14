4cec406089e4c2810097ff1d664589232875a51b
Arif Shaikh <providenceri@gmail.com>
2016-06-14 16:39:16 -0400

    made some edits

--- jax/jax/settings.py
+++ jax/jax/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jaxapp.apps.JaxappConfig',
+    'jaxapp',
 ]
 
 MIDDLEWARE_CLASSES = [
