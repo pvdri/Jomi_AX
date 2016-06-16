446afe895b72a7a529ab975e2e53bd48c26e3b10
Arif Shaikh <providenceri@gmail.com>
2016-06-16 14:42:53 -0400

    made edits to website

--- jax/jaxapp/urls.py
+++ jax/jaxapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'detail', views.detail, name='detail'),
     url(r'summary', views.summary, name='summary'),
+    url(r'ip_add', views.ip_add, name='ip_add'),
 ]
--- jax/jaxapp/views.py
+++ jax/jaxapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from models import det_ax, cli_ax
 from webform import orgform
 
+
 def home(request):
     form = orgform()
     return render(request, 'jaxapp/index.html', {'dropdownform': form})
@@ -14,8 +15,9 @@ def detail(request):
         org_id = form.cleaned_data['organization']
         orgname = cli_ax.objects.filter(id=org_id)[0].org
         detail_data = det_ax.objects.filter(org=orgname)
+        count_data = det_ax.objects.filter(org=orgname).count()
 
-    return render(request, 'jaxapp/detail_view.html', {'detail_form' : detail_data})
+    return render(request, 'jaxapp/detail_view.html', {'detail_form' : detail_data, 'ip_name' : orgname,'count' : count_data})
 
 def summary(request):
     if request.method == 'POST':
@@ -31,6 +33,20 @@ def summary(request):
       for y in monthly:
         for z in yearly:
             a = det_ax.objects.filter(org=orgname).filter(month=y["month"]).filter(year=z["year"]).count()
+            b = det_ax.objects.filter(org=orgname).filter(month=y["month"]).filter(year=z["year"]).values('ip').distinct()
             if a > 0:
-                newdict.append([orgname,": ", z["year"], " / ", y["month"], " => ", "Unique Session IDs: ", a])
-    return render(request, 'jaxapp/summary_view.html', {"sum_dict" : newdict})
+                newdict.append([y["month"], " ", z["year"], " => ", " Unique: IPs: ", len(b), "/ Session IDs:  ", a])
+    return render(request, 'jaxapp/summary_view.html', {"sum_dict" : newdict, 'ip_name' : orgname})
+
+
+def ip_add(request):
+    if request.method == 'POST':
+        form =  orgform(request.POST)
+
+    if form.is_valid():
+        org_id = form.cleaned_data['organization']
+        orgname = cli_ax.objects.filter(id=org_id)[0].org
+        ip_data = det_ax.objects.filter(org=orgname).values('ip').distinct()
+        count_data = det_ax.objects.filter(org=orgname).values('ip').distinct().count()
+
+    return render(request, 'jaxapp/ip_add.html', ({'ip_details' : ip_data, 'ip_name' : orgname, 'count' : count_data}))
